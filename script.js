88bd6ae680cda3a19642e60dfb77f23a7403d50f
evingerbono <bono0823@gmail.com>
2022-06-07 13:38:22 +0200

    megjelenites

--- script.js
+++ script.js
@@ -5,8 +5,43 @@ function beolvas(eleresiut,callback){
     fetch("foci.json")
         .then((res)=>res.json())
         .then((data)=>{
-            console.log(data.focistak);
-            callback(data.focistak);
+            console.log(data.focista);
+            callback(data.focista);
         })
         .catch((err)=>console.log(err));
+}
+function megjelenit(tomb){
+    let txt = "<table>";
+    tomb.forEach(function(foci, index) {
+        txt+="<tr id="+index+">";
+        for(const key in foci){
+            txt+="<td>";
+            txt+=foci[key];
+            txt+="</td>";
+        }
+        txt+="</tr>"
+    });
+    txt+="</table>";
+    $(".tartalom").append(txt);
+    $(".tartalom table tr").on("click", function(){
+        let aktID = $(this).attr("id")
+        console.log(aktID);
+        $(this).toggleClass("kiemel");
+        aktfocistamegjelenitese(tomb,aktID);
+    });
+
+}
+function aktfocistamegjelenitese(tomb,aktID){
+    $(".focistahely").empty();
+    let aktfocista=tomb[aktID]
+    tomb.kep
+        for(const key in aktfocista){
+            if (key=="Kep") {
+                $(".focistahely").append(`<img src="${aktfocista[key]}"alt="Focista">`);
+            }
+            else{
+                $(".focistahely").append(`<p>${aktfocista[key]}</p`);
+            }
+        }
+
 }
